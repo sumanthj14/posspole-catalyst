0728fca9ba9b4a7a84e67a29bd3b8343acef2f45
sumanthj14 <sumanthjayaprasadvvietcse@gmail.com>
2025-08-25 12:56:18 +0530

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/Desgy" : "",
-  assetPrefix: isProd ? "/Desgy/" : "",
-  output: "export",
+  basePath: isProd ? "/Posspole-catalyst" : "",
+  assetPrefix: isProd ? "/Posspole-catalyst/" : "",
+  // output: "export", // Commented out for development mode
   images: {
     unoptimized: true,
   },
